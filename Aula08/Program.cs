d2c2efa9fd152024416bf5269ddc912b15ac2f8e
akemilol <valeriaconcedsantos@gmail.com>
2025-03-25 12:58:56 -0300

    aula 08 e aula09

---
+++ Aula08/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Aula08
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Operadores Relacionais
+            Console.WriteLine("=======OPERADORES RELACIONAIS=======");
+            Console.WriteLine("Digite o primeiro número: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            // Operador de igualdade ==
+            if (x == y)
+            {
+                Console.WriteLine("Os números são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("Os números são diferentes.");
+            }
+
+            // Operador de diferença !=
+            if (x != y)
+            {
+                Console.WriteLine("Os números são diferentes.");
+            }
+            else
+            {
+                Console.WriteLine("Os números são iguais.");
+            }
+
+            // Operador de maior que >
+            if (x > y)
+            {
+                Console.WriteLine("O primeiro número é maior que o segundo.");
+            }
+            else
+            {
+                Console.WriteLine("O primeiro número não é maior que o segundo.");
+            }
+
+            // Operador de menor que <
+            if (x < y)
+            {
+                Console.WriteLine("O primeiro número é menor que o segundo.");
+            }
+            else
+            {
+                Console.WriteLine("O primeiro número não é menor que o segundo.");
+            }
+
+            // Operador de maior ou igual >=
+            if (x >= y)
+            {
+                Console.WriteLine("O primeiro número é maior ou igual ao segundo.");
+            }
+            else
+            {
+                Console.WriteLine("O primeiro número não é maior ou igual ao segundo.");
+            }
+
+            // Operador de menor ou igual <=
+            if (x <= y)
+            {
+                Console.WriteLine("O primeiro número é menor ou igual ao segundo.");
+            }
+            else
+            {
+                Console.WriteLine("O primeiro número não é menor ou igual ao segundo.");
+            }
+        }
+    }
+}
---
+++ Aula09/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=======OPERADORES LÓGICOS=======");
+        bool isLogged = true;
+        bool isAdmin = false;
+
+        Console.WriteLine("\n Informação do Usuário: ");
+        Console.WriteLine("Usuário está logado: " + isLogged);
+        Console.WriteLine("Usuário é administrador: " + isAdmin);
+
+        Console.WriteLine("\nPermissões");
+
+        // Operador || - OU
+        if (isLogged || isAdmin)
+        {
+            Console.WriteLine("\nUsuario esta logado.");
+        }
+        else
+        {
+            Console.WriteLine("\nUsuario não esta logado..");
+        }
+
+        // Operador && - E
+        if (isLogged && isAdmin)
+        {
+            Console.WriteLine("\nAcesso ao painel de administrador concedido.");
+        }
+        else
+        {
+            Console.WriteLine("\nAcesso ao painel de administrador negado.");
+        }
+
+        // Operador ! - NÃO
+        // se for true ele vai negar e vai tornar isLogged em false
+        if (!isLogged)
+        {
+            Console.WriteLine("\nUsuario não está logado");
+        }
+        else
+        {
+            Console.WriteLine("\nUsuario está logado.");
+        }
+    }
+}
