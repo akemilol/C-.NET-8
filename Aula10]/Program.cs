4761af8c36e322ebb02b88956d4595360f00f1b0
akemilol <valeriaconcedsantos@gmail.com>
2025-03-26 17:41:20 -0300

    aula 10 calculadora com if, else if, else

---
+++ Aula10]/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Aula10;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("======CALCULADORA BASICA======");
+        Console.WriteLine("Digite o primeiro número: ");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+
+        // Selecionar o tipo de operação (MENU)
+        Console.WriteLine("\nSelecione o tipo de operação: ");
+        Console.WriteLine("1 - Adição (+)");
+        Console.WriteLine("2 - Subtração (-)");
+        Console.WriteLine("3 - Multiplicação (*)");
+        Console.WriteLine("4 - Divisão (/)");
+        Console.WriteLine("Digite o número da operação desejada: ");
+        int operacao = Convert.ToInt32(Console.ReadLine());
+
+
+        double result = 0;
+
+        // Condições
+        if (operacao == 1)
+        {
+            result = n1 + n2;
+        }
+        else if (operacao == 2)
+        {
+            result = n1 - n2;
+        }
+        else if (operacao == 3)
+        {
+            result = n1 * n2;
+        }
+        else if (operacao == 4)
+        {
+            if (n2 != 0)
+            {
+                result = n1 / n2;
+            }
+            else if (n2 == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero!");
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida!");
+            }
+        }
+        Console.WriteLine("O resultado é: " + result);
+    }
+}
