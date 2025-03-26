db341d63bdd222deb5e03987b59a484ddf1bff50
akemilol <valeriaconcedsantos@gmail.com>
2025-03-26 17:53:10 -0300

    adição do while na calculadora

--- Aula10]/Program.cs
+++ Aula10]/Program.cs
@@ -2,55 +2,70 @@
 
 public class Program
 {
+
     public static void Main()
     {
-        Console.WriteLine("======CALCULADORA BASICA======");
-        Console.WriteLine("Digite o primeiro número: ");
-        double n1 = Convert.ToDouble(Console.ReadLine());
+        bool varContinue = true;
 
-        Console.WriteLine("Digite o segundo número: ");
-        double n2 = Convert.ToDouble(Console.ReadLine());
-
-        // Selecionar o tipo de operação (MENU)
-        Console.WriteLine("\nSelecione o tipo de operação: ");
-        Console.WriteLine("1 - Adição (+)");
-        Console.WriteLine("2 - Subtração (-)");
-        Console.WriteLine("3 - Multiplicação (*)");
-        Console.WriteLine("4 - Divisão (/)");
-        Console.WriteLine("Digite o número da operação desejada: ");
-        int operacao = Convert.ToInt32(Console.ReadLine());
-
-
-        double result = 0;
-
-        // Condições
-        if (operacao == 1)
+        while (varContinue)
         {
-            result = n1 + n2;
-        }
-        else if (operacao == 2)
-        {
-            result = n1 - n2;
-        }
-        else if (operacao == 3)
-        {
-            result = n1 * n2;
-        }
-        else if (operacao == 4)
-        {
-            if (n2 != 0)
+            Console.WriteLine("======CALCULADORA BASICA======");
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+
+            // Selecionar o tipo de operação (MENU)
+            Console.WriteLine("\nSelecione o tipo de operação: ");
+            Console.WriteLine("1 - Adição (+)");
+            Console.WriteLine("2 - Subtração (-)");
+            Console.WriteLine("3 - Multiplicação (*)");
+            Console.WriteLine("4 - Divisão (/)");
+            Console.WriteLine("Digite o número da operação desejada: ");
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+
+            double result = 0;
+            // Condições
+            if (operacao == 1)
             {
-                result = n1 / n2;
+                result = n1 + n2;
             }
-            else if (n2 == 0)
+            else if (operacao == 2)
             {
-                Console.WriteLine("Não é possível dividir por zero!");
+                result = n1 - n2;
             }
-            else
+            else if (operacao == 3)
             {
-                Console.WriteLine("Operação inválida!");
+                result = n1 * n2;
+            }
+            else if (operacao == 4)
+            {
+                if (n2 != 0)
+                {
+                    result = n1 / n2;
+                }
+                else if (n2 == 0)
+                {
+                    Console.WriteLine("Não é possível dividir por zero!");
+                }
+                else
+                {
+                    Console.WriteLine("Operação inválida!");
+                }
+            }
+            Console.WriteLine($"O resultado é: {result}");
+
+            Console.WriteLine("Deseja continuar? (s/n) ?");
+            String response = Console.ReadLine();
+
+            if (response == "n")
+            {
+                varContinue = false;
             }
         }
-        Console.WriteLine("O resultado é: " + result);
+        Console.WriteLine("Programa encerrado!");
     }
 }
