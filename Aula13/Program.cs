3801956ad249c1a08bbd00bb037dbb50c46a5383
akemilol <valeriaconcedsantos@gmail.com>
2025-03-26 23:04:39 -0300

    Aula 13 Do/While

---
+++ Aula13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula13;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Estrutura de repetição Do/While
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+    }
+}
