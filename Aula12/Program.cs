1def59f210765edd71f6e7d656caf58eebeab97d
akemilol <valeriaconcedsantos@gmail.com>
2025-03-26 22:58:33 -0300

    Aula 12 - While

---
+++ Aula12/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Aula 12 - While
+        // EXEMPLO 1:
+        //int i = 0;
+
+        //// a condição sempre vai estar em () e o bloco de código em {}.
+        //while (i <= 8)
+        //{
+        //    Console.WriteLine(i);
+        //    i++;
+        //}
+
+        double budget = 100;
+
+        while (budget > 0)
+        {
+            Console.WriteLine($"Você possui {budget} na carteira.");
+            // Realizando alguma operação
+            budget -= 10;
+        }
+        Console.WriteLine("Atenção saldo insuficiente");
+    }
+}
